9bbb114f22bdd0141607f8e118139bcc164bc8af
DavidHellwig <74218656+DavidHellwig@users.noreply.github.com>
2021-05-22 08:23:37 -0600

    Bug fixes
    Fixed various issues in the outdated midproject repo

---
+++ MidProjectWarMath.java
@@ -0,0 +1,10 @@
+public class MidProjectWarMath {
+    public static void main(String[] args){
+
+        CardGameCalculator calculator = new CardGameCalculator();
+        calculator.bigMath();
+
+
+
+    }
+}
